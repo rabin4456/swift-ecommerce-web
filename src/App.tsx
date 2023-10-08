7500b73414c14b59212cc0a6ff1a8aff2f1ab571
github-ananas <rabin.thapa@ananasolution.com.np>
2023-10-08 15:37:05 +0545

    tanstack query setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./utils/queryClient";
 
 function App() {
   return (
     <>
-      <RouterProvider router={routes} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={routes} />
+      </QueryClientProvider>
     </>
   );
 }
