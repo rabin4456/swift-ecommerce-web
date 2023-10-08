a648d0e9fbe5aaf95e92393841be9eef1164e802
github-ananas <rabin.thapa@ananasolution.com.np>
2023-10-08 15:31:21 +0545

    app structured modified,Router setup

---
+++ src/routes/routes.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter, Link, Navigate } from "react-router-dom";
+import Home from "../pages/home";
+import { PageRoutes } from "../global/enum";
+
+export const routes = createBrowserRouter([
+  {
+    path: PageRoutes.ROOT,
+    element: <Navigate to={PageRoutes.HOME} />,
+  },
+
+  {
+    path: PageRoutes.HOME,
+    element: <Home />,
+  },
+]);
+<Link to='/home' />;
